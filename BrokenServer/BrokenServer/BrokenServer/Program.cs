7fa091c8e8536711ab7be1729c3344c33543d26d
Carlos Cp <carlosx_1302@hotmail.com>
2014-12-03 21:17:52 -0500

    Update Broken

--- BrokenServer/BrokenServer/BrokenServer/Program.cs
+++ BrokenServer/BrokenServer/BrokenServer/Program.cs
@@ -13,10 +13,77 @@ namespace BrokenServer
     class Program
     {
         private static Thread _LoopThrreading;
-        
+        public static int VER = 1;
+        public static bool DEBUG = false;
         static void Main(string[] args)
         {
             Program pro = new Program();
+            LogConsole._Load();
+            Systems.Ini ini = null;
+
+            #region Default Settings
+            int LSPort = 00000;
+            int IPCPort = 00000;
+            string LSIP = "127.0.0.1";
+            string IPCIP = "127.0.0.1";
+
+            string m_host = "localhost";
+            string m_user = "root";
+            string m_pass = "";
+            string m_db = "";
+            int m_port = 3306;
+            #endregion
+
+            #region Load Settings
+            try
+            {
+                if (File.Exists(Environment.CurrentDirectory + @"\Settings\Settings.ini"))
+                {
+                    ini = new Systems.Ini(Environment.CurrentDirectory + @"\Settings\Settings.ini");
+                    LSPort = Convert.ToInt32(ini.GetValue("Server", "port", 12345));
+                    LSIP = ini.GetValue("Server", "ip", "127.0.0.1").ToString();
+                    VER = ini.GetValue("Server", "ver", 1);
+
+                    IPCPort = Convert.ToInt32(ini.GetValue("IPC", "port", 40706));
+                    IPCIP = ini.GetValue("IPC", "ip", "127.0.0.1").ToString();
+                    DEBUG = Convert.ToBoolean(ini.GetValue("CONSOLE", "debug", "false"));
+
+                    m_host = ini.GetValue("MySQL", "host", "localhost").ToString();
+                    m_user = ini.GetValue("MySQL", "user", "root").ToString();
+                    m_pass = ini.GetValue("MySQL", "pass", "").ToString();
+                    m_db = ini.GetValue("MySQL", "data", "").ToString();
+                    m_port = Convert.ToInt32(ini.GetValue("MySQL", "port", 3306));
+
+                    ini = null;
+                    LogConsole.Show(LogType.INFO, "Has loaded your ip settings successfully");
+                }
+                else
+                {
+                    LogConsole.Show(LogType.ALERT, "Settings.ini could not be found, using default setting");
+                }
+            }
+            catch (Exception excc)
+            {
+                LogConsole.Show(LogType.ERROR, " {0}", excc.ToString());
+                return;
+            }
+            #endregion
+
+            //Systems.Crypto.Initialize();
+            //Systems.Server net = new Systems.Server();
+            //net.OnConnect += new Systems.Server.dConnect(pro._OnClientConnect);
+            //net.OnError += new Systems.Server.dError(pro._ServerError);
+
+            //try
+            //{
+            //    net.Start(LSIP, LSPort);
+            //}
+            //catch (Exception ex)
+            //{
+            //    LogConsole.Show(LogType.ERROR, "Starting Server error: {0}", ex);
+            //}
+
+
 
             _LoopThrreading = new Thread(new ThreadStart(Program.LoopConsole));
             _LoopThrreading.Priority = ThreadPriority.BelowNormal;
---
+++ BrokenServer/BrokenServer/BrokenServer/Util/LogConsole.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace BrokenServer
+{
+    enum LogType : int
+    {
+        NORMAL = 0,
+        ERROR = 1,
+        INFO = 2,
+        ALERT = 3,
+        DEBUG = 4
+    }
+    enum LogColor : byte
+    {
+        NORMAL = 14,
+        ERROR = 15,
+        INFO = 6,
+        ALERT = 3,
+        DEBUG = 12
+    }
+
+    class LogConsole
+    {
+        static DateTime now;
+        static StackFrame frame;
+        static StackTrace trac = new StackTrace();
+        public static void _Load()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Title = "BrokenServer ";
+            Console.WriteLine("");
+            Console.WriteLine(@"                                                                                                ");
+            Console.WriteLine(@"             ______             _                   _                                           ");
+            Console.WriteLine(@"            (____  \           | |                 | |                                          ");
+            Console.WriteLine(@"             ____)  ) ____ ___ | |  _ ____ ____     \ \   ____  ____ _   _ ____  ____           ");
+            Console.WriteLine(@"            |  __  ( / ___) _ \| | / ) _  )  _ \     \ \ / _  )/ ___) | | / _  )/ ___)          ");
+            Console.WriteLine(@"            | |__)  ) |  | |_| | |< ( (/ /| | | |_____) | (/ /| |    \ V ( (/ /| |              ");
+            Console.WriteLine(@"            |______/|_|   \___/|_| \_)____)_| |_(______/ \____)_|     \_/ \____)_|              ");
+            Console.WriteLine(@"                                                                                                ");
+            Console.WriteLine(@"                                                                                                ");
+            Console.WriteLine(@"                                                                                                ");
+            Console.WriteLine("                           Copyright © CarlosX 2014");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WindowWidth = Console.LargestWindowWidth - 17;
+            Console.WindowHeight = Console.LargestWindowHeight - 23;
+        }
+        public static void Show(LogType type, string str)
+        {
+            Showb(type, str, new object[] { });
+        }
+        public static void Show(LogType type, string str, object arg0)
+        {
+            Showb(type, str, new object[] { arg0 });
+        }
+        public static void Show(LogType type, string str, object arg0, object arg1)
+        {
+            Showb(type, str, new object[] { arg0, arg1 });
+        }
+        public static void Show(LogType type, string str, object arg0, object arg1, object arg2)
+        {
+            Showb(type, str, new object[] { arg0, arg1, arg2 });
+        }
+        public static void Show(LogType type, string str, object arg0, object arg1, object arg2, object arg3)
+        {
+            Showb(type, str, new object[] { arg0, arg1, arg2, arg3 });
+        }
+        public static void Show(LogType type, string str, object arg0, object arg1, object arg2, object arg3, object arg4)
+        {
+            Showb(type, str, new object[] { arg0, arg1, arg2, arg3, arg4 });
+        }
+        public static void Showb(LogType type, string str, object[] args)
+        {
+            now = DateTime.Now;
+            string[] shortDateString = new string[] { "[", now.ToLongTimeString(), "] " };
+            frame = trac.GetFrame(2);
+
+            switch (type)
+            {
+                case LogType.NORMAL:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(string.Concat(shortDateString));
+                        Console.Write("[");
+                        Console.Write(string.Concat(frame.GetMethod().ReflectedType.Name, ".", frame.GetMethod().Name));
+                        Console.Write("] » ");
+                        Console.ForegroundColor = (ConsoleColor)LogColor.NORMAL;
+                        Console.Write(str, args);
+                        Console.Write("\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    }
+                case LogType.INFO:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(string.Concat(shortDateString));
+                        Console.Write("[");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(string.Concat(frame.GetMethod().ReflectedType.Name, ".", frame.GetMethod().Name));
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("] » ");
+                        Console.ForegroundColor = (ConsoleColor)LogColor.INFO;
+                        Console.Write(str, args);
+                        Console.Write("\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    }
+                case LogType.ALERT:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(string.Concat(shortDateString));
+                        Console.Write("[");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(string.Concat(frame.GetMethod().ReflectedType.Name, ".", frame.GetMethod().Name));
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("] » ");
+                        Console.ForegroundColor = (ConsoleColor)LogColor.ALERT;
+                        Console.Write(str, args);
+                        Console.Write("\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Beep();
+                        break;
+                    }
+                case LogType.ERROR:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(string.Concat(shortDateString));
+                        Console.Write("[");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(string.Concat(frame.GetMethod().ReflectedType.Name, ".", frame.GetMethod().Name));
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("] » ");
+                        Console.ForegroundColor = (ConsoleColor)LogColor.ERROR;
+                        Console.Write(str, args);
+                        Console.Write("\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    }
+                case LogType.DEBUG:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(string.Concat(shortDateString));
+                        Console.Write("[");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(string.Concat(frame.GetMethod().ReflectedType.Name, ".", frame.GetMethod().Name));
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("] » ");
+                        Console.ForegroundColor = (ConsoleColor)LogColor.DEBUG;
+                        Console.Write(str, args);
+                        Console.Write("\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    }
+            }
+        }
+
+        public static void HexDump(byte[] bytes, string typ, int bytesPerLine = 16)
+        {
+            if (bytes != null)
+            {
+                Show(LogType.DEBUG, typ);
+                int bytesLength = bytes.Length;
+
+                char[] HexChars = "0123456789ABCDEF".ToCharArray();
+
+                int firstHexColumn =
+                      8                   // 8 characters for the address
+                    + 3;                  // 3 spaces
+
+                int firstCharColumn = firstHexColumn
+                    + bytesPerLine * 3       // - 2 digit for the hexadecimal value and 1 space
+                    + (bytesPerLine - 1) / 8 // - 1 extra space every 8 characters from the 9th
+                    + 2;                  // 2 spaces 
+
+                int lineLength = firstCharColumn
+                    + bytesPerLine           // - characters to show the ascii value
+                    + Environment.NewLine.Length; // Carriage return and line feed (should normally be 2)
+
+                char[] line = (new String(' ', lineLength - 2) + Environment.NewLine).ToCharArray();
+                int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
+                StringBuilder result = new StringBuilder(expectedLines * lineLength);
+
+                for (int i = 0; i < bytesLength; i += bytesPerLine)
+                {
+                    line[0] = HexChars[(i >> 28) & 0xF];
+                    line[1] = HexChars[(i >> 24) & 0xF];
+                    line[2] = HexChars[(i >> 20) & 0xF];
+                    line[3] = HexChars[(i >> 16) & 0xF];
+                    line[4] = HexChars[(i >> 12) & 0xF];
+                    line[5] = HexChars[(i >> 8) & 0xF];
+                    line[6] = HexChars[(i >> 4) & 0xF];
+                    line[7] = HexChars[(i >> 0) & 0xF];
+
+                    int hexColumn = firstHexColumn;
+                    int charColumn = firstCharColumn;
+
+                    for (int j = 0; j < bytesPerLine; j++)
+                    {
+                        if (j > 0 && (j & 7) == 0) hexColumn++;
+                        if (i + j >= bytesLength)
+                        {
+                            line[hexColumn] = ' ';
+                            line[hexColumn + 1] = ' ';
+                            line[charColumn] = ' ';
+                        }
+                        else
+                        {
+                            byte b = bytes[i + j];
+                            line[hexColumn] = HexChars[(b >> 4) & 0xF];
+                            line[hexColumn + 1] = HexChars[b & 0xF];
+                            line[charColumn] = (b < 32 ? '·' : (char)b);
+                        }
+                        hexColumn += 3;
+                        charColumn++;
+                    }
+                    result.Append(line);
+                }
+                //return result.ToString();
+                Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
